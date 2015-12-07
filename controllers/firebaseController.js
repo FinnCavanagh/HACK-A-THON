f7c3f87c47281123a4f3ba91406f4bfd0948234c
siyounguk <si.younguk@gmail.com>
2015-12-07 10:59:58 +0000

    chat from friday & sign in

---
+++ controllers/firebaseController.js
@@ -0,0 +1,56 @@
+angular
+  .module("myapp")
+  .controller("firebaseController", firebaseController);
+
+function firebaseController($firebaseArray, $scope, $firebaseAuth) {
+  var firebaseRef = new Firebase("https://blistering-inferno-6696.firebaseio.com")
+  
+  console.log($scope)
+
+  $scope.user = { email: "", password: "" };
+  $scope.messages = $firebaseArray(firebaseRef);
+    
+  $scope.addMessage = function(e) {
+    if (e.keyCode != 13) return;
+    $scope.messages.$add({from: $scope.name, body: $scope.msg});
+    $scope.msg = "";
+  }
+
+  $scope.createUser = function(){
+     console.log("In create user");
+     firebaseRef.createUser({
+       email    : $scope.user.email,
+       password : $scope.user.password
+     }, 
+    function(error, userData) {
+       if (error) {
+         console.log("Error creating user:", error);
+        }else{
+         console.log("Successfully created user account with uid:", userData.uid);
+       }
+    });
+  }
+
+  $scope.authWithPassword = function(){
+    firebaseRef.authWithPassword({
+    email    : $scope.user.email,
+    password : $scope.user.password
+    }, 
+    function(error, authData) {
+     if (error) {
+       console.log("Login Failed!", error);
+      }else {
+       console.log("Authenticated successfully with payload:", authData);
+       
+      }
+    }); 
+  }
+
+  $scope.token = function(){
+    console.log(authData)
+  }
+
+
+
+   var auth = $firebaseAuth
+}
