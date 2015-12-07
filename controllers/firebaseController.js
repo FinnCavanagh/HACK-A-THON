5190fe5b22e35339d458f10895c66de1ad75bfc8
siyounguk <si.younguk@gmail.com>
2015-12-07 11:24:01 +0000

    chat working

--- controllers/firebaseController.js
+++ controllers/firebaseController.js
@@ -3,52 +3,53 @@ angular
   .controller("firebaseController", firebaseController);
 
 function firebaseController($firebaseArray, $scope, $firebaseAuth) {
-  var firebaseRef = new Firebase("https://blistering-inferno-6696.firebaseio.com")
+  var firebaseRef = new Firebase("https://burning-inferno-8419.firebaseio.com")
   
-  console.log($scope)
+
 
   $scope.user = { email: "", password: "" };
   $scope.messages = $firebaseArray(firebaseRef);
     
   $scope.addMessage = function(e) {
+    console.log($scope)
     if (e.keyCode != 13) return;
     $scope.messages.$add({from: $scope.name, body: $scope.msg});
     $scope.msg = "";
   }
 
-  $scope.createUser = function(){
-     console.log("In create user");
-     firebaseRef.createUser({
-       email    : $scope.user.email,
-       password : $scope.user.password
-     }, 
-    function(error, userData) {
-       if (error) {
-         console.log("Error creating user:", error);
-        }else{
-         console.log("Successfully created user account with uid:", userData.uid);
-       }
-    });
-  }
+  // $scope.createUser = function(){
+  //    console.log("In create user");
+  //    firebaseRef.createUser({
+  //      email    : $scope.user.email,
+  //      password : $scope.user.password
+  //    }, 
+  //   function(error, userData) {
+  //      if (error) {
+  //        console.log("Error creating user:", error);
+  //       }else{
+  //        console.log("Successfully created user account with uid:", userData.uid);
+  //      }
+  //   });
+  // }
 
-  $scope.authWithPassword = function(){
-    firebaseRef.authWithPassword({
-    email    : $scope.user.email,
-    password : $scope.user.password
-    }, 
-    function(error, authData) {
-     if (error) {
-       console.log("Login Failed!", error);
-      }else {
-       console.log("Authenticated successfully with payload:", authData);
+  // $scope.authWithPassword = function(){
+  //   firebaseRef.authWithPassword({
+  //   email    : $scope.user.email,
+  //   password : $scope.user.password
+  //   }, 
+  //   function(error, authData) {
+  //    if (error) {
+  //      console.log("Login Failed!", error);
+  //     }else {
+  //      console.log("Authenticated successfully with payload:", authData);
        
-      }
-    }); 
-  }
+  //     }
+  //   }); 
+  // }
 
-  $scope.token = function(){
-    console.log(authData)
-  }
+  // $scope.token = function(){
+  //   console.log(authData)
+  // }
 
 
 
